d75c90ef8b6038af89126b1fb4a73bc0cd273c7c
vishant007 <vishant9797@gmail.com>
2022-12-19 18:34:48 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,25 @@
+import Navbar from './Navbar';
+import Billing from './Billing';
+import Carddeal from './Carddeal';
+import Business from './Business';
+import CTA from './CTA';
+import Stats from './Stats';
+import Footer from './Footer';
+import Testimonials from './Testimonials';
+import Hero from './Hero';
+import Feedback from './Feedback';
+import Button from './Button';
+
+export {
+	Navbar,
+	Billing,
+	Button,
+	Carddeal,
+	Business,
+	CTA,
+	Stats,
+	Footer,
+	Testimonials,
+	Hero,
+	Feedback,
+};
